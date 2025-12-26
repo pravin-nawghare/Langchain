6169e2c9f497bc3056870d3d189ad21f9b76129e
pravin-nawghare <nawgharepraveen@gmail.com>
2025-12-26 22:11:00 +0530

    data ingestion functionality added

--- RAG/components/data_ingestion.py
+++ RAG/components/data_ingestion.py
@@ -1,6 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader, PyMuPDFLoader, CSVLoader, TextLoader, DirectoryLoader
 from pathlib import Path
-from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 from typing import List, Any, Optional
 import numpy as np
@@ -10,45 +9,39 @@ class DataIngestion:
         #self.path = path
         print('Data Ingestion process started')
     
-    def _read_data(self,path_of_folder) -> List:
+    def _read_data(self,path_of_folder:str) -> List[Any]:
         print('Reading the folder path')
-        folder_path = Path(path_of_folder)
+        documents = []
+        folder_path = Path(path_of_folder).resolve()
+        print(f"Data path: {folder_path}")
+
         if folder_path == '':
             print(f"Path not provided")
             return None
-        loader = DirectoryLoader(
-            path= folder_path,
-            glob = '*.pdf', # provide pattern to load file. for text or csv or pdf -> give the respected pattern
-            loader_cls= PyPDFLoader # provide what type of loader is like text, pdf, etc
-        )
-        load_file = loader.load() # after try lazy_load()
-        return load_file
+        
+        # For pdf files
+        pdf_files = list(folder_path.glob(''))
+        print(f"Found {len(pdf_files)} pdf files: {[str(file) for file in pdf_files]}")
 
-    def _create_chunks(self,document):
-        if document == None:
-            print("No document provided for chunking")
-            return None
-        text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size = 20,
-                chunk_overlap = 0,
-                separators= ['\n\n', '\n', ' ', '']
-            )
-        text_chunks = text_splitter.split_documents(document)
-        return text_chunks
+        for file in pdf_files:
+            print(f'Loading file: {file}')
+            try:
+                loader = PyPDFLoader(str(file))
+                load_file = loader.load()
+                print(f"Loaded {len(load_file)} documents from {file}")
+                documents.extend(load_file)
+            except Exception as e:
+                print(f"Error loading {file}: {e}")
 
-    def _create_embeddings(self,chunks) -> np.array:
-        if chunks == None:
-            print("No text chunks were provided for embedding")
-            return None
-        embedding_model = SentenceTransformer(
-            'sentence-transformers/all-MiniLM-L6-v2'
-        )
-        embeddings = embedding_model.encode(chunks)
-        return embeddings
+        # loader = DirectoryLoader(
+        #     path= folder_path,
+        #     glob = '*.pdf', # only pdf files from root directory. '**/*' -> all files.
+        #     loader_cls= PyPDFLoader # provide what type of loader is like text, pdf, etc
+        # )
+        return documents
+
 
     def data_ingestion_stage(self,path:str):
-        data = self._read_data(path)
-        document = self._create_chunks(data)
-        embeddings = self._create_embeddings(document)
-        return embeddings
+        document = self._read_data(path)
+        return document
 
