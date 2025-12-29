fa1258f0f2f38ba274b0473996633dabdd71ea34
pravin-nawghare <nawgharepraveen@gmail.com>
2025-12-29 15:20:24 +0530

    file reading code updated

--- RAG/components/data_ingestion.py
+++ RAG/components/data_ingestion.py
@@ -1,47 +1,43 @@
-from langchain_community.document_loaders import PyPDFLoader, PyMuPDFLoader, CSVLoader, TextLoader, DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
 from pathlib import Path
-from sentence_transformers import SentenceTransformer
-from typing import List, Any, Optional
-import numpy as np
+from typing import List, Any
 
 class DataIngestion:
-    def __init__(self):
-        #self.path = path
+    def __init__(self,path:str):
+        self.path = path
         print('Data Ingestion process started')
     
-    def _read_data(self,path_of_folder:str) -> List[Any]:
-        print('Reading the folder path')
-        documents = []
-        folder_path = Path(path_of_folder).resolve()
-        print(f"Data path: {folder_path}")
-
-        if folder_path == '':
+    def _read_data(self) -> List[Any]:
+        if self.path == '':
             print(f"Path not provided")
             return None
         
+        print('Reading the directory path')
+        documents = []
+        directory_path = Path(self.path).resolve()
+        print(f"Directory path: {directory_path}")
+        
         # For pdf files
-        pdf_files = list(folder_path.glob(''))
-        print(f"Found {len(pdf_files)} pdf files: {[str(file) for file in pdf_files]}")
+        pdf_files = list(directory_path.glob('**/*.pdf'))
+        print(f"Found {len(pdf_files)} pdf files: {[str(file).split('\\')[-1] for file in pdf_files ]}")
 
-        for file in pdf_files:
-            print(f'Loading file: {file}')
-            try:
+        for file in pdf_files: 
+            print(f'Loading file: {[str(file).split('\\')[-1]]}')
+            try: 
                 loader = PyPDFLoader(str(file))
                 load_file = loader.load()
-                print(f"Loaded {len(load_file)} documents from {file}")
+                print(f"Loaded {len(load_file)} documents from {[str(file).split('\\')[-1]]}")
                 documents.extend(load_file)
             except Exception as e:
                 print(f"Error loading {file}: {e}")
 
-        # loader = DirectoryLoader(
-        #     path= folder_path,
-        #     glob = '*.pdf', # only pdf files from root directory. '**/*' -> all files.
-        #     loader_cls= PyPDFLoader # provide what type of loader is like text, pdf, etc
-        # )
         return documents
 
 
-    def data_ingestion_stage(self,path:str):
-        document = self._read_data(path)
+    def data_ingestion_stage(self):
+        document = self._read_data()
         return document
 
+# data_ingestion = DataIngestion(path="./RAG/data")
+# docs = data_ingestion.data_ingestion_stage()
+# print(docs[3].page_content)
